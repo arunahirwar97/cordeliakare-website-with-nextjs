1e2c9b6f43d67c80cf50ac83dba183e0160c1c6e
Abhishek-Tripathy <134616533+Abhishek-Tripathy@users.noreply.github.com>
2025-07-09 20:38:18 +0530

    build issue fixed.

--- components/ScrollTop.tsx
+++ components/ScrollTop.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function ScrollToTop() {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
 
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [pathname, searchParams])
+  }, [pathname]) 
 
   return null
 }
